3029404b6233e536432b67ad4cf6f5b216e4f7df
blessedmadukoma <blessedmadukoma@gmail.com>
2025-07-13 11:16:02 +0200

    update deployment script

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  basePath: process.env.NODE_ENV === "production" ? "/blessedmadukoma" : "",
-  assetPrefix: process.env.NODE_ENV === "production" ? "blessedmadukoma" : "",
+  basePath: "",
+  assetPrefix: "",
   images: {
     domains: [
       "res.cloudinary.com",
