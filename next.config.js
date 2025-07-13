02e79bfbf7e43529c1b19702f0f2bf883e067044
blessedmadukoma <blessedmadukoma@gmail.com>
2025-07-13 11:06:13 +0200

    switched to nextjs

--- next.config.js
+++ next.config.js
@@ -1,11 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
+  basePath: process.env.NODE_ENV === "production" ? "/blessedmadukoma" : "",
+  assetPrefix: process.env.NODE_ENV === "production" ? "blessedmadukoma" : "",
   images: {
     domains: [
       "res.cloudinary.com",
       "cdn.simpleicons.org",
       "upload.wikimedia.org",
     ],
+    unoptimized: true,
   },
 };
 
